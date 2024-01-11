e669a44d058c22706e24b9eb2d1ffc89422a8db9
Wei Xiang <chehi716@gmail.com>
2024-01-11 10:45:34 +0800

    HomePage Done

--- src/Homepage/Homepage.js
+++ src/Homepage/Homepage.js
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react"
 
 export default function Homepage() {
     const [windowHeight, setwindowHeight] = useState(window.innerHeight);
+    const [ButtonImage, setButtonImage] = useState("Pause");
 
     const handleResize = () => {
         setwindowHeight(window.innerHeight);    
@@ -19,13 +20,31 @@ export default function Homepage() {
         }
     },[windowHeight])
 
+    const Pause = () => {
+        var Content = document.getElementById("Gordon");
+        if(Content.paused) {
+            Content.play();
+            setButtonImage("Pause");
+        } else{
+            Content.pause();
+            setButtonImage("Play");
+        }
+    }
+
+    function Unmute() {
+        var Video = document.getElementById("Gordon");
+        Video.muted = !Video.muted;
+    }
+
     return(
         <>
             <Header />
                 <div className="First-Content" id="First-Content">
-                    <video autoPlay muted>
+                    <video id="Gordon" autoPlay muted onClick={() => Unmute()}> 
                         <source src="./IT'SFKINGRAW.mp4"></source>
                     </video>
+                    <button id="Pause" className={ButtonImage} onClick={() => Pause()}></button>
+                    {/* <button></button> */}
                 </div>
                 <div className="Content Second">
                     <h1>
@@ -33,6 +52,17 @@ export default function Homepage() {
                     </h1>
                     <p>Cook it fresh, Eat it fresh</p>
                     <button id="Share">Share</button>
+                </div>
+                <div className="Content Last">
+                    <div className="Context">
+                        <h1 data-text="Shopping">Shopping</h1>
+                    </div>
+                    <div className="Context">
+                        <h1 data-text="Favorited">Favorited</h1>
+                    </div>
+                    <div className="Middle">
+                        <h1>Freshly Dropped</h1>
+                    </div>
                 </div>
             <Tab />
         </>
